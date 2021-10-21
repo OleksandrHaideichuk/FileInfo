da12bdfff67ddada24204e3c052c056206179942
OleksandrHaideichuk <oleksandr.haideichuk@gmail.com>
2021-10-21 22:32:22 +0300

    добавив dir

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,7 +14,7 @@ public class Main {
 
         // створив папки і файли
 
-        File directory = new File("Folder1");
+        File directory = new File("C:\\Users\\ПК\\Desktop\\COD\\Folder1");
 
         File file1 = new File("Folder1/file1.txt");
         File file2 = new File("Folder1/file2.txt");
@@ -30,20 +31,34 @@ public class Main {
         }
         System.out.println(directory.getAbsolutePath());
     }
-    /* скопіював код з посилання яке ти кидав, намагався зрозуміти як привязати це ( інші варіанти теж пробував)
+    /* скопіював код з посилання яке ти кидав, намагався зрозуміти як привязати це (інші варіанти теж пробував)
     до папки яку я створив */
 
     public Set<String> listFilesUsingDirectoryStream(String dir) throws IOException {
+
+        dir = "C:\\Users\\ПК\\Desktop\\COD\\Folder1";
+
         Set<String> fileList = new HashSet<>();
+
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(dir))) {
-            for (Path Folder1 : stream) {
-                if (!Files.isDirectory(Folder1)) {
-                    fileList.add(Folder1.getFileName()
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    fileList.add(path.getFileName()
                             .toString());
+
+
+
                 }
+
             }
+
+            System.out.println(fileList);
+            
         }
         return fileList;
+
     }
+
+
 
 }
